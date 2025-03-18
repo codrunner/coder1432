5a25e0f341d7101f3978674978c564b5744adfad
codrunner <rkp8862822124@gmail.com>
2025-03-18 18:00:42 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,24 @@
+// primitive data type 
+// 7 type:String,Number,Boolean,Null, undefined,symbol,BigInt
+
+//reference(none primitive data type):array,objecct,function
+
+const id=symbol('123');
+const anotheId=symbol('123');
+console.log(id===anotherID);
+// value will not be same.... 
+
+//arrays
+let arrays=[saktiman, rathor,naagraj];
+
+//object
+let myObj={
+    name:"ranjit kumar",
+    age:22,
+
+}
+
+//function..
+let myFunction=function(){
+    console.log("hello world");
+}
