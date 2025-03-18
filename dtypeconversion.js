0852f00e733f30b9e30250b2f170cf3d03836075
codrunner <rkp8862822124@gmail.com>
2025-03-18 16:53:50 +0000

    conversion of ddatatype

---
+++ dtypeconversion.js
@@ -0,0 +1,20 @@
+let score="23abc"
+console.log(typeof(score));
+valueNumber=Number(score);
+console.log(typeof(valueNumber));
+console.log(valueNumber); //output NaN(NOT A NUMBER)
+
+//"33"=>33
+//"33ABC"=>NaN
+//true => 1; false => 0
+let isLoggedIn=1
+let booleanIsloggedIn=Boolean(isLoggedIn);
+console.log(booleanIsloggedIn);
+// i  => true; 0 => false
+// " 0" => false
+// "ranjit" => true
+
+let someNumber=88
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
